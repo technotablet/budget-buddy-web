46e6ce9f60a3cdcbeeb4ef9da7101e7177d5f456
Vivek Kapoor <vivek.kapoor@1digitalstack.ai>
2024-03-04 21:44:44 +0530

    Added pug templating engine. Views created for Dashboard and Profile Listing.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const { auth, requiresAuth } = require('express-openid-connect');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,16 +18,19 @@ const config = {
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
+// Set PUG templating engine
+app.set('views', path.join(__dirname, 'views')); // Set the directory for Pug templates
+app.set('view engine', 'pug'); // Use Pug as the template engine
+
+
 // req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
-  let loggedIn = "Logged In. <a href='/profile'>Profile</a> • <a href='/logout'>Logout</a>";
-  let loggedOut = "Logged Out. <a href='/login'>Login</a>";
-  res.send(req.oidc.isAuthenticated() ? loggedIn : loggedOut);
+  res.render('index', { title: "Dashboard", isAuthenticated: req.oidc.isAuthenticated(), user: req.oidc.user });
 });
 
 app.get('/profile', requiresAuth(), (req, res) => {
-    res.send(JSON.stringify(req.oidc.user));
-  });
+  res.render('profile', { title: "User Profile", isAuthenticated: req.oidc.isAuthenticated(), user: req.oidc.user });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
