7284865cc027e12845e865bc4772c51b5f644a13
Vivek Kapoor <vivek.kapoor@gmail.com>
2024-03-10 11:54:37 +0530

    Implemented auto refresh of access token (using refreshToken and built-in refresh() method)

--- routes/defaultRoutes.js
+++ routes/defaultRoutes.js
@@ -4,17 +4,28 @@ const { requiresAuth } = require('express-openid-connect');
 
 
 // req.isAuthenticated is provided from the auth router
-router.get('/', (req, res) => {
-    // special page that should work without authentication also (no requiresAuth())
-    let accessToken = req.oidc?.accessToken?.access_token || "";
+router.get('/', async (req, res) => {
 
-    res.render('index', { title: "Dashboard", accessToken: accessToken, isAuthenticated: req.oidc.isAuthenticated(), user: req.oidc.user });
+    let access_token; // Declare access_token at a higher scope
+    if (req.oidc && req.oidc.accessToken) {
+        // special page that should work without authentication also (no requiresAuth())
+        let accessTokenDetails = req.oidc.accessToken;
+        if (accessTokenDetails.isExpired()) {
+            ({ access_token } = await accessTokenDetails.refresh());
+        } else {
+            access_token = accessTokenDetails.access_token;
+        }
+    }
+
+    res.render('index', { title: "Dashboard", accessToken: access_token, isAuthenticated: req.oidc.isAuthenticated(), user: req.oidc.user });
 });
 
-router.get('/profile', requiresAuth(), (req, res) => {
-    let accessToken = req.oidc.accessToken;
-    console.log(req.oidc.refreshToken);
-    res.render('profile', { title: "User Profile", accessToken: accessToken, isAuthenticated: req.oidc.isAuthenticated(), user: req.oidc.user });
+router.get('/profile', requiresAuth(), async (req, res) => {
+    let { token_type, access_token, isExpired, refresh } = req.oidc.accessToken;
+    if (isExpired()) {
+        ({ access_token } = await refresh());
+    }
+    res.render('profile', { title: "User Profile", accessToken: access_token, isAuthenticated: req.oidc.isAuthenticated(), user: req.oidc.user });
 });
 
 router.get('/add', requiresAuth(), (req, res) => {
